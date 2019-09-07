2a8e2f36173cad5104ae19e4597599f32a776229
kangwenxuan <miriplek@outlook.com>
2019-09-07 19:17:45 +0800

    base commit

---
+++ dataLoader.py
@@ -0,0 +1,80 @@
+from torch.utils.data import DataLoader, Dataset, sampler
+from maskFuncs import *
+from albumentations import (HorizontalFlip, ShiftScaleRotate, Normalize, Resize, Compose, GaussNoise)
+import cv2
+import os
+from albumentations.torch import ToTensor
+from sklearn.model_selection import train_test_split
+
+class SteelDataset(Dataset):
+    def __init__(self, df, data_folder, mean, std, phase):
+        self.df = df
+        self.root = data_folder
+        self.mean = mean
+        self.std = std
+        self.phase = phase
+        self.transforms = get_transforms(phase, mean, std)
+        self.fnames = self.df.index.tolist()
+
+    def __getitem__(self, idx):
+        image_id, mask = make_mask(idx, self.df)
+        image_path = os.path.join(self.root, "train_images", image_id)
+        img = cv2.imread(image_path)
+        augmented = self.transforms(image=img, mask=mask)
+        img = augmented['image']
+        mask = augmented['mask']  # 1x256x1600x4
+        mask = mask[0].permute(2, 0, 1)  # 1x4x256x1600
+        return img, mask
+
+    def __len__(self):
+        return len(self.fnames)
+
+
+def get_transforms(phase, mean, std):
+    list_transforms = []
+    if phase == "train":
+        list_transforms.extend(
+            [
+                HorizontalFlip(),  # only horizontal flip as of now
+            ]
+        )
+    list_transforms.extend(
+        [
+            Normalize(mean=mean, std=std, p=1),
+            ToTensor(),
+        ]
+    )
+    list_trfms = Compose(list_transforms)
+    return list_trfms
+
+
+def provider(
+        data_folder,
+        df_path,
+        phase,
+        mean=None,
+        std=None,
+        batch_size=8,
+        num_workers=4,
+):
+    '''Returns dataloader for the model training'''
+    df = pd.read_csv(df_path)
+    # some preprocessing
+    # https://www.kaggle.com/amanooo/defect-detection-starter-u-net
+    df['ImageId'], df['ClassId'] = zip(*df['ImageId_ClassId'].str.split('_'))
+    df['ClassId'] = df['ClassId'].astype(int)
+    df = df.pivot(index='ImageId', columns='ClassId', values='EncodedPixels')
+    df['defects'] = df.count(axis=1)
+
+    train_df, val_df = train_test_split(df, test_size=0.2, stratify=df["defects"])
+    df = train_df if phase == "train" else val_df
+    image_dataset = SteelDataset(df, data_folder, mean, std, phase)
+    dataloader = DataLoader(
+        image_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=True,
+        shuffle=True,
+    )
+
+    return dataloader
---
+++ maskFuncs.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv('../input/train.csv')
+
+
+def mask2rle(img):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+
+def rle2mask(mask_rle, shape=(1600, 256)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (width,height) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T
+
+def make_mask(row_id, df):
+    '''Given a row index, return image_id and mask (256, 1600, 4)'''
+    fname = df.iloc[row_id].name
+    labels = df.iloc[row_id][:4]
+    masks = np.zeros((256, 1600, 4), dtype=np.float32) # float32 is V.Imp
+    # 4:class 1～4 (ch:0～3)
+
+    for idx, label in enumerate(labels.values):
+        if label is not np.nan:
+            label = label.split(" ")
+            positions = map(int, label[0::2])
+            length = map(int, label[1::2])
+            mask = np.zeros(256 * 1600, dtype=np.uint8)
+            for pos, le in zip(positions, length):
+                mask[pos:(pos + le)] = 1
+            masks[:, :, idx] = mask.reshape(256, 1600, order='F')
+    return fname, masks
+
+# Test RLE functions
+assert mask2rle(rle2mask(train['EncodedPixels'].iloc[0])) == train['EncodedPixels'].iloc[0]
+assert mask2rle(rle2mask('1 1')) == '1 1'
